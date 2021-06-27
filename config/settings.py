5bc64371b216a20f7cfe4b42f2b701c9e3a78076
JonniUz <javoxirbek.uzb@gmail.com>
2021-06-28 00:25:20 +0500

    It is finished

--- config/settings.py
+++ config/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.core',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
